9b71cd47c54cc2c2293e8a6b5c367da71befb34f
Gaurav Raj Sinha <gauravraj.sinha@singleinterface.com>
2020-10-21 11:05:34 +0530

    settings changes

--- timeline/settings.py
+++ timeline/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path.cwd()
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +84,7 @@ WSGI_APPLICATION = "timeline.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": str(BASE_DIR / "db.sqlite3")
     }
 }
 
